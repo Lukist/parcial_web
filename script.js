d6716f10963a7ee4e25f2ce596ac424383daf95d
PalaciosLucas <lucaspalacios@gmail.com>
2023-11-09 15:03:35 -0300

    primera version

---
+++ script.js
@@ -0,0 +1,64 @@
+var story = {
+	currentScene: "Especalidades",
+	Especalidades: {
+		img: `<svg xmlns="http://www.w3.org/2000/svg" width="80px" height="80px" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M19 2.01H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.998 5 19.815 5 19.01c0-.101.009-.191.024-.273.112-.575.583-.717.987-.727H20c.018 0 .031-.009.049-.01H21V4.01c0-1.103-.897-2-2-2zm0 14H5v-11c0-.806.55-.988 1-1h7v7l2-1 2 1v-7h2v12z"></path></svg>`,
+		title: "Conocimiento",
+		story: `Tengo conocimientos en varias áreas de la programación. Me caracterizo en el desarrollo para aplicaciones móviles para el sistema operativo android, y cuento con conocimientos full stack para el desarrollo de páginas web con Java y SQL.
+		<br><br> Programas Móviles: <a href="https://github.com/Lukist/Humming_notes.git">Notas/Recordatorio</a>, <a href="https://github.com/Lukist/RepaairDev.git">Registro de reparaciones</a>
+		<br><br> Aplicaciones Web: <a href="https://github.com/Lukist/Parque-de-diversiones-full-stack.git">Parque de Diversiones</a>
+		<br><br> Certificaciones: <a href="https://drive.google.com/file/d/1ASGdL1ktU5MjLrbZtVBKzANuvR_nYAcZ/view?usp=sharing">Programacion full stack</a>`,
+		nextScene: "Experiencia"
+	  },
+	Experiencia: {
+		img: `<svg xmlns="http://www.w3.org/2000/svg" width="100px" height="100px" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M9.37 17.51c-3.09.86 1.87 2.64 5.8 1a7.18 7.18 0 0 1-1.1-.54 11.59 11.59 0 0 1-4.16.18c-1.31-.15-.54-.64-.54-.64zm5.33-1.68a16.27 16.27 0 0 1-5.36.26c-1.31-.14-.45-.77-.45-.77-3.4 1.12 1.89 2.4 6.64 1a2.25 2.25 0 0 1-.83-.49zm1.82-9.73s-6.87 1.71-3.59 5.49a1.47 1.47 0 0 1-.25 2.12s2.46-1.27 1.33-2.86-1.86-2.22 2.51-4.75z"></path><path d="M18.13 19.14s.56.46-.63.82c-2.27.69-9.43.9-11.42 0-.72-.31.62-.74 1-.83a2.54 2.54 0 0 1 .69-.08c-.8-.56-5.15 1.1-2.21 1.58 8.05 1.37 14.64-.56 12.57-1.49zm-2.76-4.88a7 7 0 0 1 .92-.49s-1.51.27-3 .4a28.11 28.11 0 0 1-4.82.05C6.09 13.91 9.74 13 9.74 13a6.81 6.81 0 0 0-3.16.75c-2.06 1.03 5.09 1.48 8.79.51zm.91 2.44a.39.39 0 0 1-.08.09c5-1.32 3.18-4.65.78-3.81a.71.71 0 0 0-.33.25s.14-.05.43-.12c1.22-.25 2.92 1.63-.8 3.59zM12 13c-.74-1.67-3.26-3.14 0-5.71C16.09 4.08 14 2 14 2c.84 3.31-3 4.31-4.34 6.37-.93 1.41.47 2.92 2.34 4.63z"></path><path d="M16.41 21.32a29.75 29.75 0 0 1-9.14.14s.46.38 2.84.53c3.61.23 9.15-.12 9.29-1.83 0 0-.26.64-2.99 1.16z"></path></svg>`,
+		title: "Experiencia",
+		story: `Poseo experiencia en aplicaciones donde se implica diseño de interfaces UI/UX, manejo de base de datos de forma local, y funcionalidades especializadas a petición del cliente. <br><br> Projectos especializados: <a href="https://www.figma.com/file/JcgkrVb7g8PStirXiNoNJI/Untitled?type=design&mode=design&t=rc1sepFxvceXrZ9l-1">Programa de gestion de empresas</a>, <a href="https://www.figma.com/file/EZhH2HrEb8zHOqyI4gd1mj/farmacia_app?type=design&mode=design&t=rc1sepFxvceXrZ9l-1">Programa de gestion de medicamentos</a>`,
+		nextScene: "Redes"
+	},
+	Redes: {
+		img: `<svg xmlns="http://www.w3.org/2000/svg" width="80px" height="80px" viewBox="0 0 24 24" style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;"><path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path><circle cx="16.806" cy="7.207" r="1.078"></circle><path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path></svg>`,
+		title: `Redes`,
+		story: `Gracias por su tiempo. Si desea conocer o siente curiosidad por más de mis proyectos, contácteme por alguno de los siguientes medios: <br><br> Instagram: <a href="https://www.instagram.com/lucas_s_palacios/">@lucas_s_palacios</a>
+		<br><br> Github: <a href="https://github.com/Lukist">Lukist</a>
+		<br><br> Correo: lucaspalacios.lsp@gmail.com`
+	}
+		  
+}		 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+	var button = document.querySelector("#btn");
+	button.addEventListener("click", renderScene, false);
+  });
+  
+  function renderScene() {
+	if (story[story.currentScene]) {
+	  var text = "Siguente";
+	  if (story[story.currentScene].buttonText) {
+		text = story[story.currentScene].buttonText;
+	  }
+	  var content = document.querySelector("#container").innerHTML = `
+	  	${story[story.currentScene].img}
+		<h2 class="title">${story[story.currentScene].title}</h2>
+		<p class="story">${story[story.currentScene].story}</p>
+		<button class="btn" id="next-button">${text}</button>
+	  `;
+	  var button = document.querySelector("#next-button");
+
+	  if (story[story.currentScene].nextScene) {
+		button.addEventListener("click", function () {
+			renderScene();
+		  });
+	  }else {
+		button.style.display = "none";
+	  }
+	  
+	  
+	  story.currentScene = story[story.currentScene].nextScene;
+	}
+	
+	
+  }
+
+  
+  
